6e7c65be093674604d8beee6aeebbdf26c099deb
dzarflo <slarkai@byupathway.edu>
2025-07-15 00:31:04 +0000

    Completed week 1 assignment 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -5,5 +5,45 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        {
+            DisplayWelcome();
+            string name = PromptUserName();
+            int number = PromptUserNumber();
+
+            int squaredNumber = SquareNumber(number);
+
+            DisplayReult(name, squaredNumber);
+        }
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("Age: ");
+            string ageString = Console.ReadLine();
+            int age = int.Parse(ageString);
+            return age;
+        }
+
+        static int SquareNumber(int age)
+        {
+            int square = age * age;
+            return square;
+        }
+
+        static void DisplayReult(string name, int square)
+        {
+            Console.WriteLine($"{name}, the square of your number is {square}");
+        }
+
     }
 }
