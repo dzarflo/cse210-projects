91c7833cdba7d313aba6934a9d9e3cfa2ec4bf8f
dzarflo <slarkai@byupathway.edu>
2025-07-18 18:50:29 +0000

    Completed the Abstraction activity

---
+++ week02/Resumes/Job.cs
@@ -0,0 +1,18 @@
+// Hint: This is the public class Job syntax
+public class Job
+{
+    public string _company;
+    public string _jobTitle;
+    public int _startYear;
+    public int _endYear;
+
+    public Job()
+    {
+    }
+
+    public void DisplayJobDetails()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
+
