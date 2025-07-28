a03f485474ee0c48b69d3f2b588e8d4360c53ef0
dzarflo <slarkai@byupathway.edu>
2025-07-28 14:48:02 +0000

    completed the week 03 activity

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,5 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+
+
+        Fraction division1 = new Fraction();
+        Fraction division2 = new Fraction(6);
+        Fraction division3 = new Fraction(6, 7);
+
+        Console.WriteLine(division1.GetFractionString());
+        Console.WriteLine(division1.GetDecimalValue());
+        
+        Console.WriteLine(division2.GetFractionString());
+        Console.WriteLine(division2.GetDecimalValue());
+
+        Console.WriteLine(division3.GetFractionString());
+        Console.WriteLine(division3.GetDecimalValue());
     }
 }
